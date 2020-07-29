e51eed6f1cef9cb4729f2abab88f7ab474b56a63
sarkarrohit001 <sarkar.rohit01@gmail.com>
2020-07-29 19:47:35 +0530

    commit

---
+++ Loan Calculator/app.js
@@ -0,0 +1,62 @@
+
+const formvar = document.getElementById('loan-form');
+formvar.addEventListener('submit', calculateResults);
+
+function calculateResults(e)
+{
+
+    console.log("e");
+
+const amount =  document.getElementById('amount');
+const interest = document.getElementById('interest');
+const years = document.getElementById('years');
+const monthPay = document.getElementById('monthly-payment');
+const totalPayment = document.getElementById('total-payment');
+const totalInterest = document.getElementById('total-interest');
+
+const principal = parseFloat(amount.value);
+const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+const calculatedPayment = parseFloat(years.value) * 12;
+
+const x = Math.pow(1+calculatedInterest, calculatedPayment);
+const monthly = (principal*x*calculatedInterest) / (x-1);
+
+if(isFinite(monthly))
+{
+    monthPay.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayment).toFixed(2);
+    totalInterest.value = ((monthly*calculatedPayment) - principal).toFixed(2);
+
+}
+
+else{
+
+    showError('Please check your numbers');
+
+
+}
+
+e.preventDefault();
+
+}
+
+
+
+
+function showError(error)
+{
+    const errDiv = document.createElement('div');
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+    errDiv.className = 'alert alert-danger';
+    errDiv.appendChild(document.createTextNode(error));
+
+    card.insertBefore(errDiv, heading);
+
+    setTimeout(clearError, 3000);
+}
+
+function clearError()
+{
+    document.querySelector('.alert').remove();
+}
