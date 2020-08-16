c9e1b13f7f08e8ad004a1a686bbb857a8a6959bd
sarkarrohit001 <sarkar.rohit01@gmail.com>
2020-08-16 23:44:39 +0530

    commit

--- ToDoApp/public/browser.js
+++ ToDoApp/public/browser.js
@@ -1,5 +1,25 @@
+// Delete Feature
 document.addEventListener("click", function(e) {
+
+    //console.log(e.target.classList);
+if (e.target.classList.contains("delete-me")) {
+
+    //alert("Here");
+    if (confirm("Do you really want to delete this item permanently?")) {
+      axios.post('/delete-item', {id: e.target.getAttribute("data-id")}).then(function () {
+        e.target.parentElement.parentElement.remove()
+      }).catch(function() {
+        console.log("Please try again later.")
+      })
+    }
+  }
+
+
+
+
+//document.addEventListener("click", function(e) {
     if (e.target.classList.contains("edit-me")) {
+        //alert("Here");
       let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
       if (userInput) {
         axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function () {
