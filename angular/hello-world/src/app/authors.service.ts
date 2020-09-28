f8150799be2e535b470103f75dc45dd2080d6403
sarkarrohit001 <sarkar.rohit01@gmail.com>
2020-09-28 21:43:48 +0530

    commit

---
+++ angular/hello-world/src/app/authors.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorsService {
+
+  constructor() { }
+  getAuthors(){
+    return ["Shakespeare", "Charles", "Leo"]
+  }
+}
